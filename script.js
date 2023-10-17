75e64e7b6e26edb66f50388b39235fa2514dc1a1
JohnBadri <135007003+JohnBadri@users.noreply.github.com>
2023-10-17 13:53:36 +1100

    Final product, Etch-a-Sketch, simplicity is perfection

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
 const sketchBox = document.querySelector(".sketchContainer");
-const buttons = document.querySelectorAll("button");
+const sizeButtons = document
+  .querySelector(".sizeButtons")
+  .querySelectorAll("button");
+const colorButtons = document
+  .querySelector(".colorButtons")
+  .querySelectorAll("button");
 
-buttons.forEach((button) => {
+sizeButtons.forEach((button) => {
   button.addEventListener("click", () => {
     let width, height;
     width = button.id == "28" ? "28px" : button.id == "50" ? "50px" : "70px";
     height = button.id == "28" ? "28px" : button.id == "50" ? "50px" : "70px";
     changeSketchBox(button.id, width, height);
+  });
+});
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+colorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.id == "black"
+      ? sketchBox.addEventListener("mouseover", (div) => {
+          if (div.target.classList.contains("childBox")) {
+            div.target.style.backgroundColor = "#000";
+          }
+        })
+      : button.id == "white"
+      ? sketchBox.addEventListener("mouseover", (div) => {
+          if (div.target.classList.contains("childBox")) {
+            div.target.style.backgroundColor = "#fff";
+          }
+        })
+      : button.id == "colorful"
+      ? sketchBox.addEventListener("mouseover", (div) => {
+          if (div.target.classList.contains("childBox")) {
+            const randomColor = getRandomColor();
+            div.target.style.backgroundColor = randomColor;
+          }
+        })
+      : null;
   });
 });
 
